59e3e6682a1687636335c9f41616f7d55971c67b
LucasBoaretto <lcboaretto@hotmail.com>
2025-05-13 11:37:48 -0300

    feito exercicio 36

--- CursoJS - CFB Cursos/Aula36/aula36.js
+++ CursoJS - CFB Cursos/Aula36/aula36.js
@@ -1,6 +1,6 @@
 const caixa1 = document.getElementById("caixa1");
 const caixa2 = document.getElementById("caixa2");
-const btn = document.getElementById("btn_copiar");
+const btn = document.getElementById("btn_transferir");
 const todosCursos = [...document.querySelectorAll(".curso")];
 
 todosCursos.map((el) => {
@@ -12,7 +12,12 @@ todosCursos.map((el) => {
 
 btn.addEventListener("click", () => {
   const cursosSelecionados = [...document.querySelectorAll(".selecionado")];
+  const cursosNaoSelecionados = [...document.querySelectorAll(".curso:not(.selecionado)")];
+
   cursosSelecionados.map((el) => {
     caixa2.appendChild(el);
+  });
+  cursosNaoSelecionados.map((el) => {
+    caixa1.appendChild(el);
   });
 });
