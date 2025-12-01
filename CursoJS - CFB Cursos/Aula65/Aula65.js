2c2fe6ad76573a0962f5b3cd7643f6f366edba41
Lucas Boaretto <lucas.boaretto@tradicao.coop.br>
2025-12-01 09:11:19 -0300

    Adicionadas aulas 65 e 66

---
+++ CursoJS - CFB Cursos/Aula65/Aula65.js
@@ -0,0 +1,16 @@
+const pessoa ={
+    nome: 'Bruno',
+    canal: 'CFB Cursos',
+    curso: 'JavaScript',
+    aulas: {
+        aula01: 'Introdução',
+        aula02: 'Variáveis',
+        aula03: 'Condicional'
+    }
+}
+const string_pessoa = '{"nome":"Bruno","canal":"CFB Cursos","curso":"JavaScript","aulas":{"aula01":"Introdução","aula02":"Variáveis","aula03":"Condicional"}}'
+const s_json_pessoa = JSON.stringify(pessoa) // Converte Objeto em string JSON
+const o_json_pessoa = JSON.parse(string_pessoa) // Converte string JSON em Objeto
+console.log(pessoa)
+console.log(s_json_pessoa)
+console.log(o_json_pessoa)
