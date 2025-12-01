0a0ebe7eea20c517c1fa6fd44d182f0a31a27517
LucasBoaretto <lcboaretto@hotmail.com>
2025-12-01 19:57:21 -0300

    incrementado o timer com estilo aos botoes e adicionados inputs de hora, minuto e segundo

--- CursoJS - CFB Cursos/Timer JS/Timer.js
+++ CursoJS - CFB Cursos/Timer JS/Timer.js
@@ -2,7 +2,9 @@ const div_data = document.getElementById("div_data");
 const div_relogio = document.getElementById("div_relogio");
 const btn_ativar = document.getElementById('btn_ativar');
 const btn_parar = document.getElementById('btn_parar');
-const tmp_alarme = document.getElementById('tmp_alarme');
+const tmp_alarme_hours = document.getElementById('tmp_alarme_hours');
+const tmp_alarme_minutes = document.getElementById('tmp_alarme_minutes');
+const tmp_alarme_seconds = document.getElementById('tmp_alarme_seconds');
 const hora_alarme = document.getElementById('hora_alarme');
 const timer = document.getElementById("timer");
 
@@ -15,18 +17,29 @@ let alarme_ativado = false;
 let alarme_tocando = false;
 
 btn_ativar.addEventListener("click", () => {
+    alarme_tocando = false;
     ts_atual = Date.now();
-    ts_alarme = ts_atual + (tmp_alarme.value * 1000);
+    ts_alarme = ts_atual + (tmp_alarme_hours.value * 3600000) + (tmp_alarme_minutes.value * 60000) + (tmp_alarme_seconds.value * 1000);
     alarme_ativado = true;
     const dt_alarme = new Date(ts_alarme);
-    hora_alarme.innerHTML = "Hora do Alarme:" + dt_alarme.getHours() + ":" + dt_alarme.getMinutes() + ":" + dt_alarme.getSeconds();
+
+    let hora = dt_alarme.getHours();
+    hora = hora < 10 ? "0" + hora : hora;
+    let minuto = dt_alarme.getMinutes();
+    minuto = minuto < 10 ? "0" + minuto : minuto;
+    let segundo = dt_alarme.getSeconds();
+    segundo = segundo < 10 ? "0" + segundo : segundo;
+
+    hora_alarme.innerHTML = "Hora do Alarme:" + hora + ":" + minuto + ":" + segundo;
 })
 
 btn_parar.addEventListener("click", () => {
     alarme_ativado = false;
     alarme_tocando = true;
     hora_alarme.innerHTML = "Hora do Alarme:";
-    tmp_alarme.value = 0;
+    tmp_alarme_hours.value = 0;
+    tmp_alarme_minutes.value = 0;
+    tmp_alarme_seconds.value = 0;
     timer.classList.remove("alarme");
     som_alarme.pause();
     som_alarme.currentTime = 0;
@@ -53,11 +66,11 @@ const relogio = () => {
     segundo = segundo < 10 ? "0" + segundo : segundo;
     const hora_completa = hora + ":" + minuto + ":" + segundo;
     div_relogio.innerHTML = hora_completa
-    if(alarme_ativado && !alarme_tocando){
-        if(data.getTime() >= ts_alarme){
+    if (alarme_ativado && !alarme_tocando) {
+        if (data.getTime() >= ts_alarme) {
             alarme_tocando = true;
             som_alarme.play();
-            timer.classList.add("alarme")
+            timer.classList.add("alarme");
         }
     }
 
